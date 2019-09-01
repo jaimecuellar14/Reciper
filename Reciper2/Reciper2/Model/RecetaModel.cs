4923b7e3816d252840a8ecdf76b6c9e9d6251657
Jaime Cuellar <jamescuellar14@gmail.com>
2019-09-01 18:06:24 +0200

    Mostrar informacion de receta seleccionada

--- Reciper2/Reciper2/Model/RecetaModel.cs
+++ Reciper2/Reciper2/Model/RecetaModel.cs
@@ -10,5 +10,20 @@ namespace Reciper2.Model
         public string imagen { get; set; }
         public string calorias { get; set; }
 
+        public List<string> ingredientes { get; set; }
+        public List<string> especificacionesDieteticas { get; set; }
+        public List<string> especificacionesSanitarias { get; set; }
+
+
+        public RecetaModel(string Nombre, string Imagen, string Calorias, List<string> Ingredientes, 
+            List<string> dieta, List<string> saludable)
+        {
+            this.nombre = Nombre;
+            this.imagen = Imagen;
+            this.calorias = Calorias;
+            this.ingredientes = Ingredientes;
+            this.especificacionesDieteticas = dieta;
+            this.especificacionesSanitarias = saludable;
+        }
     }
 }
--- Reciper2/Reciper2/View/RecetaView.xaml.cs
+++ Reciper2/Reciper2/View/RecetaView.xaml.cs
@@ -23,24 +23,36 @@ namespace Reciper2.View
         ObservableCollection<RecetaModel> recetasList = new ObservableCollection<RecetaModel>();
         public ObservableCollection<RecetaModel> RecetasList { get { return recetasList; } }
 
-
+        private bool 
+            _cargando;
+        public bool cargando { get { return _cargando; } }
         public RecetaView()
         {
             InitializeComponent();
             this.BindingContext = this;
             listadoRecetas.ItemsSource = recetasList;
-            getRecipes();
+            //getRecipes();
+            //OnAppearing();
 
 
         }
-       /* protected async override void OnAppearing()
+        protected async override void OnAppearing()
          {
-             getRecipes();
-             base.OnAppearing();
-         }*/
+             var stack = Navigation.NavigationStack;
+                if (stack.Count == 3)
+                {
+                    getRecipes();
+                }
+            base.OnAppearing();
+         }
         public async void getRecipes()
         {
 
+            loadingIndicator.IsEnabled = true;
+            loadingIndicator.IsRunning = true;
+            loadingIndicator.IsVisible = true;
+            loadingText.IsVisible = true;
+            indicaciones.IsVisible = false;
             var tipoReceta = this.BindingContext as RecetasModel;
             if(tipoReceta != null)
             {
@@ -79,19 +91,47 @@ namespace Reciper2.View
                         var jsonString = jsonDoc.ToString();
                         dynamic objetoRecetas = JsonConvert.DeserializeObject(jsonString);
                         var listadoRecetas = objetoRecetas.hits;
-                        foreach(dynamic receta in listadoRecetas)
+                        foreach (dynamic receta in listadoRecetas)
                             {
                             dynamic rec = receta.recipe;
                             string nombre = rec.label;
                             string imagen = rec.image;
                             string calorias = rec.calories;
-                            recetasList.Add(new RecetaModel { nombre = nombre, imagen= imagen, calorias = calorias});
+                            List<string> listaIngredientes = new List<string>();
+                            List<string> listaDieta = new List<string>();
+                            List<string> listaSaludable = new List<string>();
+                            foreach (var ingredientes in rec.ingredientLines)
+                            {
+                                listaIngredientes.Add(ingredientes.Value);
+                            }
+                            foreach(var dieta in rec.dietLabels)
+                            {
+                                listaDieta.Add(dieta.Value);
+                            }
+                            foreach(var saludable in rec.healthLabels)
+                            {
+                                listaSaludable.Add(saludable.Value);
+                            }
+                            recetasList.Add(new RecetaModel(nombre, imagen, calorias, listaIngredientes, listaDieta, listaSaludable));
                             }
                         // await DisplayAlert("Alert", jsonDoc, "OK");
                      }
-                 }
+                    loadingIndicator.IsEnabled = false;
+                    loadingIndicator.IsRunning = false;
+                    loadingIndicator.IsVisible = false;
+                    loadingText.IsVisible = false;
+                    indicaciones.IsVisible = true;
+                }
 
             }
+        }
+
+        private async void ListadoRecetas_ItemTapped(object sender, ItemTappedEventArgs e)
+        {
+            RecetaModel recetaSeleccionada = e.Item as RecetaModel;
+            InformacionReceta informacionReceta = new InformacionReceta();
+            informacionReceta.BindingContext = recetaSeleccionada;
+            await Navigation.PushAsync(informacionReceta, true);
         }
     }
 }
